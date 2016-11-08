4a7f99f79cb5a72cdd249d6950df53031ef339ce
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-08 14:55:45 +0100

    Removed useless files

--- build_normal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-qmake travis_qmake_gcc_cpp11_qt_qtest.pro
-make
-# ./travis_qmake_gcc_cpp11_qt_qtest # Do not run on Travis
--- build_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-qmake travis_qmake_gcc_cpp11_qt_qtest_test.pro
-make
-./travis_qmake_gcc_cpp11_qt_qtest_test
